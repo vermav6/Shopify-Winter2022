c5f1e72b0411c1dcd480285abaf0eb8cdda27a99
vermav6 <35265457+vermav6@users.noreply.github.com>
2021-09-10 22:16:37 -0400

    Added masonry wall for initial images

--- front-end/src/components/Home.vue
+++ front-end/src/components/Home.vue
@@ -1,62 +1,140 @@
 <template>
-  <div>
-    <h1 style="color: white !important">Shopify x ImagIn</h1>
-    <div
-      id="cardHolder"
-      class="row row-cols-1 row-cols-sm-3 row-cols-md-4 g-3 mt-3 mb-3"
-    >
-      <div class="col" v-for="item in 10" :key="item">
-        <div class="card text-white bg-dark h-100 text-center">
-          <img
-            class="card-img-top"
-            style="max-height: 50vh; object-fit: contain"
-            alt="..."
-            src="https://firebasestorage.googleapis.com/v0/b/shopify-winter2022.appspot.com/o/1631246904790?alt=media&token=bcb2cafb-1114-4a8d-a2d2-2453ea5b6e01"
-          />
-          <div class="card-body">
-            <h5 class="card-title">
-              <b>Hello</b>
-            </h5>
-            <!-- <p class="card-text">{{ item.description }}</p> -->
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Uploaded at xyz</small>
-          </div>
-        </div>
+  <masonry-wall class="mason" :items="items" :column-width="300" :padding="14">
+    <template #default="{ item }">
+      <div class="card">
+        <img v-lazy="item.img" />
+        <h1>{{ item.title }}</h1>
+        <span>{{ item.description }}</span>
       </div>
-    </div>
-  </div>
+    </template>
+  </masonry-wall>
 </template>
 
 <script>
 export default {
-  name: "Home",
   data() {
-    return {};
+    return {
+      items: [
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://firebasestorage.googleapis.com/v0/b/shopify-winter2022.appspot.com/o/1631246904790?alt=media&token=bcb2cafb-1114-4a8d-a2d2-2453ea5b6e01",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/1200/1250",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/900/1520",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/5100/2150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/450/1750",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/500/150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/680/170",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/602/190",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/610/170",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/620/157",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/610/195",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/600/150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/600/150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/600/150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/600/150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/600/150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/600/150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/600/150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/600/150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/600/150",
+        },
+        {
+          title: "First",
+          description: "The first item.",
+          img: "https://random.imagecdn.app/600/150",
+        },
+      ],
+    };
   },
-  methods: {},
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.card {
+  background-color: rgb(251, 247, 237) !important;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+img {
+  max-width: 100% !important;
+  max-height: 500px !important;
+  object-fit: contain;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-
-#cardHolder {
-  margin-left: 20px;
-  margin-right: 20px;
+.mason {
+  padding: 40px !important;
 }
 </style>
--- front-end/src/main.js
+++ front-end/src/main.js
@@ -1,10 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import {router} from "@/plugins/router"
+import MasonryWall from '@yeger/vue-masonry-wall'
+import VueLazyLoad from 'vue3-lazyload'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap"
 
 const app = createApp(App)
 app.use(router)
+app.use(MasonryWall)
+app.use(VueLazyLoad, {
+    preLoad: 1.3,
+    error: 'https://www.globalsign.com/application/files/9516/0389/3750/What_Is_an_SSL_Common_Name_Mismatch_Error_-_Blog_Image.jpg',
+    loading: 'https://i.giphy.com/media/N256GFy1u6M6Y/giphy.webp',
+    attempt: 2
+  })
 app.mount('#app')
