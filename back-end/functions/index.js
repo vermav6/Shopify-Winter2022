e0470ec812a41abc3104002cedd73082627f1dd8
vermav6 <35265457+vermav6@users.noreply.github.com>
2021-09-18 18:29:25 -0400

    Added test script

--- back-end/functions/index.js
+++ back-end/functions/index.js
@@ -107,3 +107,52 @@ async function updateFileMetadata(storageRef, fileName, metadata) {
     return null;
   }
 }
+
+/**
+ * @param {string} url
+ * @param {array<string>} classifications
+ */
+function testClarifai(url, classifications) {
+  // eslint-disable-next-line new-cap
+  stub.PostModelOutputs(
+      {
+        model_id: "aaa03c23b3724a16a56b629203edc62c",
+        inputs: [{data: {image: {url: url}}}],
+      },
+      metadata,
+      async (err, response) => {
+        if (err || response.status.code !== 10000) {
+          return;
+        }
+
+        const final = [];
+        for (const c of response.outputs[0].data.concepts) {
+          if (c.value >= 0.9) {
+            final.push(c.name);
+          }
+        }
+        let classificationStatus=true;
+        for (const iterator of classifications) {
+          if (!final.includes(iterator)) {
+            classificationStatus = false;
+          }
+        }
+        for (const iterator of final) {
+          if (!classifications.includes(iterator)) {
+            // eslint-disable-next-line max-len
+            classificationStatus = false;
+          }
+        }
+        if (!classificationStatus) {
+          console.log("Testing Clarifai API integration failed");
+        } else {
+          console.log("Testing Clarifai API integration successful");
+        }
+      },
+  );
+}
+
+if (process.env.FUNCTIONS_EMULATOR === true ||
+  process.env.FUNCTIONS_EMULATOR === "true") {
+  testClarifai("https://www.zdnet.com/a/hub/i/r/2021/01/07/a20ae151-6384-47c4-a75e-802455021c41/thumbnail/1200x900/87979d415e6537a431386a8120ae95b1/apple-iphone-12-best-phones-review.png", ["no person", "plastic", "one", "technology", "business", "electronics", "isolated"]);
+}
